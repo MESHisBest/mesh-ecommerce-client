9cc125d5969018d696f64d867559d511c4938126
Your Name <you@example.com>
2021-01-28 10:45:32 -0600

    Styling and BUtton Logic Format Carousel Margin bottom to product cards Logic that displays button based on user sign-in by Han and Enrique

--- src/components/AllProducts/CarouselProducts.js
+++ src/components/AllProducts/CarouselProducts.js
@@ -3,11 +3,9 @@ import Carousel from 'react-bootstrap/Carousel'
 import products from '../../data/products-list'
 
 const cardContainerLayout = {
-  width: '99%',
-  height: '25%',
+  width: '80%',
+  height: '18%',
   // borderColor: 'blue',
-  borderRadius: '25px',
-  border: '2px solid blue',
   padding: '15px',
   margin: '10px'
 }
@@ -18,41 +16,43 @@ const dblock = {
 
 const CarouselProducts = () => {
   return (
-    <Carousel style={cardContainerLayout}>
-      <Carousel.Item>
-        <img
-          src={products[0].pictureUrl}
-          // className="d-block w-100"
-          style={dblock}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>iPhone 12</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          src={products[1].pictureUrl}
-          // className="d-block w-100"
-          style={dblock}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Macbook Pro</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          // className="d-block w-100"
-          style={dblock}
-          src={products[2].pictureUrl}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>HP Monitor</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Carousel style={cardContainerLayout}>
+        <Carousel.Item>
+          <img
+            src={products[0].pictureUrl}
+            // className="d-block w-100"
+            style={dblock}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>iPhone 12</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={products[1].pictureUrl}
+            // className="d-block w-100"
+            style={dblock}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Macbook Pro</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            // className="d-block w-100"
+            style={dblock}
+            src={products[2].pictureUrl}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>HP Monitor</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   )
 }
 // <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
