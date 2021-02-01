d49ca6bb0e2a9ce9d6c43ddca25e597fb99e13e3
Han Nguyen <hanvnguyen94@gmail.com>
2021-02-01 11:11:08 -0500

    fixing heroku link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://damp-falls-64365.herokuapp.com/',
+  production: 'https://damp-falls-64365.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
